a3f0b03b63f74265223f7567eee4cc41c7a88ed6
hohohosn <day7of01@gmail.com>
2025-06-11 23:20:34 +0900

    1-3 Validation

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -8,6 +8,8 @@ import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import jakarta.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +23,7 @@ public class UserController {
     }
 
     @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+    public void changePassword(@Auth AuthUser authUser, @Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
         userService.changePassword(authUser.getId(), userChangePasswordRequest);
     }
 }
--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    //비밀번호 유효성 검사
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에는 최소 한 개의 숫자가 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에는 최소 한 개의 대문자가 포함되어야 합니다.")
+
     @NotBlank
     private String newPassword;
 }
